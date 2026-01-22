8a480263e4c71834a513882e946df4c3b06a72a0
Soumi Pal <soumiisc2020@gmail.com>
2026-01-22 12:20:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// Basic console usage
+// 1. Create two variables: one storing your full name and another
+// storing your favorite hobby. Print a sentence in the format:
+// My name is <your name> and <your hobby>"
+// const nm = "Soumi Pal"
+// const hbby = "reading books"
+// console.log("My name is " + nm + " and my hobby is " + hbby)
+
+
+// 2. Perform the arithmetic calculation `45 * 2 - 10` and
+// print the final result
+// const result = 45 * 2 - 10;
+// console.log(result);
+
+
+// 3. Write a program that retrieves and prints the current year
+// using the Date object in JavaScript
+// const currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+
+// 4. Store your first name and last name in two different
+// variables, then print your full name in a single output
+// const nm = "Soumi"
+// const tt = "Pal"
+// console.log(nm + " " + tt);
+
+
+// 5. Create a variable with an initial value. Print its
+// value, and print the updated value again
+// let a = 10;
+// console.log(a);
+// a += 1;
+// console.log(a);
+
+
+// 6. Print a custom error message using `console.error()`
+// console.error("This is an error message");
+
+
+// 7. Store a number in a variable and print the square of
+// that number
+// let num = 5;
+// console.log(num * num);
+
+
+// 8. Create a boolean variable and print its value
+// let isJavaScriptFun = true;
+// console.log(isJavaScriptFun);
+
+
+// 9. Store your age in a variable and print whether your
+// age is greater than 18
+// let age = 21
+// if(age > 18){
+//     console.log("Age greater than 21");
+// }
+// else{
+//     console.log("Age less than 21");
+// }
+
+// 10. Divide `100` by `0` and print the result. Observe
+// what JavaScript returns
+// const result = 100 / 0;
+// console.log(result); // Outputs: Infinity
+
+// Variables and Data Types
+
